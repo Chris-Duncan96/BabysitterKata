2d494616a755e83f77f49ca7a4d8a0836f627278
cmduncan <cmduncan@iastate.edu>
2017-12-20 16:55:01 -0600

    Added bedtime rate and implementation

--- src/babysitterCalculator/babysitterCalculator.java
+++ src/babysitterCalculator/babysitterCalculator.java
@@ -4,20 +4,25 @@ public class babysitterCalculator {
 
 	private static int baseRate = 8;
 	private static int afterMidnightBonusRate = 8;
+	private static int beforeBedBonusRate = 4;
 
 	private static int CalculatePaymentByHoursAndRate(int hours, int rate) {
 		return rate*Math.max(hours, 0);
 	}
 
-	public static int CalculatePaymentByStartEndTimes(int startTime, int endTime) {
+	public static int CalculatePayment(int startTime, int bedTime, int endTime) {
 		startTime = addTimeIfPastMidnight(startTime);
+		bedTime = addTimeIfPastMidnight(bedTime);
 		endTime = addTimeIfPastMidnight(endTime);
-		return CalculatePaymentByHoursAndRate(endTime - startTime, baseRate) + CalculatePaymentByHoursAndRate(endTime - 12, afterMidnightBonusRate);
+		bedTime = Math.min(12, bedTime);
+		return CalculatePaymentByHoursAndRate(endTime - startTime, baseRate) + CalculatePaymentByHoursAndRate(endTime - 12, afterMidnightBonusRate)
+																			 + CalculatePaymentByHoursAndRate(bedTime - startTime, beforeBedBonusRate);
 	}
 
 	private static int addTimeIfPastMidnight(int time) {
 		if(time < 5) time+=12;
 		return time;
 	}
+
 	
 }
--- src/babysitterCalculatorTests/BabySitterCalculatorTests.java
+++ src/babysitterCalculatorTests/BabySitterCalculatorTests.java
@@ -9,30 +9,44 @@ import org.junit.jupiter.api.Test;
 class BabySitterCalculatorTests {
 	
 	@Test
-	void testCalculatePaymentByStartEndTimesReturns56WhenGiven5to12() {
-		int actualDollarsEarned = babysitterCalculator.CalculatePaymentByStartEndTimes(5,12);
+	void testCalculatePaymentWhenGiven5to12WithBedAt5Returns56() {
+		int actualDollarsEarned = babysitterCalculator.CalculatePayment(5, 5, 12);
 		int expectedDollarsEarned = 56;
 		assertEquals(expectedDollarsEarned, actualDollarsEarned);
 	}
 	
 	@Test
-	void testCalculatePaymentByStartEndTimesReturns40WhenGiven6to11() {
-		int actualDollarsEarned = babysitterCalculator.CalculatePaymentByStartEndTimes(6,11);
+	void testCalculatePaymentWhenGiven6to11WithBedAt6Returns40() {
+		int actualDollarsEarned = babysitterCalculator.CalculatePayment(6,6,11);
 		int expectedDollarsEarned = 40;
 		assertEquals(expectedDollarsEarned, actualDollarsEarned);
 	}
 	
 	@Test
-	void testCalculatePaymentByStartEndTimesReturns16WhenGiven12to1() {
-		int actualDollarsEarned = babysitterCalculator.CalculatePaymentByStartEndTimes(12,1);
+	void testCalculatePaymentWhenGiven12to1WithBedAt12Returns16() {
+		int actualDollarsEarned = babysitterCalculator.CalculatePayment(12,12,1);
 		int expectedDollarsEarned = 16;
 		assertEquals(expectedDollarsEarned, actualDollarsEarned);
 	}
 	
 	@Test
-	void testCalculatePaymentByStartEndTimesReturns24WhenGiven11to1() {
-		int actualDollarsEarned = babysitterCalculator.CalculatePaymentByStartEndTimes(11,1);
+	void testCalculatePaymentWhenGiven11to1WithBedAt11Returns24() {
+		int actualDollarsEarned = babysitterCalculator.CalculatePayment(11,11,1);
 		int expectedDollarsEarned = 24;
+		assertEquals(expectedDollarsEarned, actualDollarsEarned);
+	}
+	
+	@Test
+	void testCalculatePaymentReturnsWhenGiven10to12WithBedAt11Returns20() {
+		int actualDollarsEarned = babysitterCalculator.CalculatePayment(10,11,12);
+		int expectedDollarsEarned = 20;
+		assertEquals(expectedDollarsEarned, actualDollarsEarned);
+	}
+	
+	@Test
+	void testCalculatePaymentReturnsWhenGiven12to4WithBedAt1Returns64() {
+		int actualDollarsEarned = babysitterCalculator.CalculatePayment(12,1,4);
+		int expectedDollarsEarned = 64;
 		assertEquals(expectedDollarsEarned, actualDollarsEarned);
 	}
 
